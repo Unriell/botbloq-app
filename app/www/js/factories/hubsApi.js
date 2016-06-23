1500ff0ee42ff6eda6db6512f58b2a1e35de96e4
JorgeGarciaIrazabal <jorge.garcia@bq.com>
2016-06-23 08:28:40 +0200

    find intel process

--- app/www/js/factories/hubsApi.js
+++ app/www/js/factories/hubsApi.js
@@ -7,7 +7,7 @@
  *
  * */
 angular.module('botbloq')
-    .factory('hubsApi', function ($q, $cordovaDeviceMotion, $websocket, toaster) {
+    .factory('hubsApi', function ($rootScope, $q, $cordovaDeviceMotion, $websocket, toaster, $ionicPlatform) {
         function webSocketWrapper(url) {
             var ws = $websocket(url);
             ws.onMessage(function (ev) {
@@ -22,26 +22,34 @@ angular.module('botbloq')
             return ws;
         }
 
+        function checkIp(ip, waiting) {
+            console.log("checking ip: ", ip);
+            api.connect('ws://' + ip + ':8889').then(function () {
+                var ipFound = 'ws://' + ip + ':8889';
+                toaster.clear([waiting]);
+                toaster.success('Connected', "Intel found in: " + ip);
+                api.connect(ipFound, 2000)
+                    .then(function () {
+                        api.UtilsAPIHub.server.setId("mobile");
+                    });
+            })
+        }
 
         var api = new HubsAPI(15000, webSocketWrapper, $q);
-        api.connect("ws://127.0.0.1:8889", 2000)
-            .then(function () {
-                api.UtilsAPIHub.server.setId("mobile");
-            });
 
-        api.onClose = function (error){
+        api.onClose = function (error) {
             console.error("closed", error);
         };
-        api.onOpen = function (){
+        api.onOpen = function () {
             console.log("open");
         };
-        api.onReconnecting = function (error){
+        api.onReconnecting = function (error) {
             console.warn("reconnecting", error);
         };
-        api.onMessageError = function (error){
+        api.onMessageError = function (error) {
             console.error("message error", error);
         };
-        api.onClientFunctionNotFound = function (hub, func){
+        api.onClientFunctionNotFound = function (hub, func) {
             console.error("closed", hub, func);
         };
 
@@ -50,8 +58,36 @@ angular.module('botbloq')
         };
 
         api.TopicsManager.client.showToast = function (message) {
-            toaster.pop('success', "received message", message);
+            toaster.info("received message", message);
         };
+
+        api.findRosServer = function (ip) {
+            var ipBase = ip.split('.'),
+                waiting = toaster.wait({
+                    title: 'Searching',
+                    body: 'Looking for intel IP',
+                    timeout: 10000
+                });
+            for (var i = 1; i < 256; i++) {
+                ipBase[3] = i;
+                var current_ip = ipBase.join('.');
+                checkIp(current_ip, waiting);
+            }
+        };
+
+        $ionicPlatform.ready(function () {
+            if (window.networkinterface) {
+                networkinterface.getIPAddress(function (ip) {
+                    alert(ip);
+                }, function (error){
+                    console.error(error);
+                });
+            } else {
+                api.findRosServer('192.168.43.159');
+            }
+        }, false);
+
+
 
         return api;
     });
